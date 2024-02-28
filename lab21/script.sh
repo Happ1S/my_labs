b54e8a77598d9a1b7242c788e2b8e529af9129d9
Happ1S <116806077+Happ1S@users.noreply.github.com>
2024-02-28 15:31:58 +0300

    Add files via upload

---
+++ lab21/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [[ "$1" == "?" ]]; then
+	echo "
+Имя:		script
+Версия:         1.0
+
+Описание:
+	Этот скрипт удаляет файлы с заданным расширением и заданным кол-вом дней,
+	 в течение которых к файлу не обращались.
+
+Подсказка:
+	1-ый параметр отвечает за расширение (Пример: .txt)
+	2-ой параметр отвечает за кол-во дней (Пример: 3)
+"
+	exit 0;
+fi;
+
+suffix="${1:-.txt}";
+days=${2:-0};
+mapfile -t files < <(find . -type f -atime $days | xargs ls -ld | grep -oE '[^ /]+$')
+
+for file in "${files[@]}"; do
+	answer=$(echo "$file" | grep -E "\\$suffix");
+	if [[ $answer != "" ]]; then
+		# rm $answer;
+		echo $answer;
+	fi;
+done;
+
